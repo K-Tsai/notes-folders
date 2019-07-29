23731fcebade46f5de936969ebdfa248c53f9e13
Kevin Tsai <k.tsai92@gmail.com>
2019-07-29 15:50:37 -0400

    addedfolders/notes

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import NotefulForm from '../NotefulForm/NotefulForm'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './AddFolder.css'
 
 export default class AddFolder extends Component {
   static defaultProps = {
@@ -7,11 +11,37 @@ export default class AddFolder extends Component {
     },
   }
   static contextType = ApiContext;
+  handleSubmit = event => {
+    event.preventDefault()
+    const folder = {
+      name: event.target['folderInput'].value
+    }
+    fetch(`${config.API_ENDPOINT}/folders`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(folder),
+    })
+    .then(res => {
+      if (!res.ok)
+        return res.json().then(e => Promise.reject(e))
+      return res.json()
+    })
+    .then(folder => {
+      this.context.addFolder(folder)
+      this.props.history.push(`/folder/${folder.id}`)
+    })
+    .catch(error => {
+      console.error( {error} )
+    })
+  }
 
   render() {
     return (
       <section className='AddFolder'>
         <h2>Create a folder</h2>
+        <NotefulForm onSubmit={this.handleSubmit}>
           <div className='form'>
             <label htmlFor='folderInput'>
               Enter a folder name: 
@@ -23,6 +53,7 @@ export default class AddFolder extends Component {
               Add folder
             </button>
           </div>
+        </NotefulForm>
       </section>
     )
   }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import NotefulForm from '../NotefulForm/NotefulForm'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './AddNote.css'
+
 
 export default class AddFolder extends Component {
   static defaultProps = {
@@ -8,13 +13,41 @@ export default class AddFolder extends Component {
   }
 	static contextType = ApiContext;
 	
-	handleSubmit(event) {
-		event.preventDefault();
-	}
+	handleSubmit = event => {
+    event.preventDefault();
+    const newNote = {
+      name: event.target['note-name-input'].value,
+      content: event.target['note-content-input'].value,
+      folderId: event.target['folder-choice'].value,
+      modified: new Date(),
+    }
+    fetch(`${config.API_ENDPOINT}/notes`, {
+      method:'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newNote),
+    })
+    .then(res => {
+      if(!res.ok)
+        return res.json().then(e => Promise.reject(e))
+        return res.json()
+    })
+    .then(note => {
+      this.context.addNote(note)
+      this.props.history.push(`/folder/${note.folderId}`)
+    })
+    .catch(error => {
+      console.error( {error} )
+    })
+  }
+
   render() {
+    const {folders = [] } = this.context
     return (
       <section className='AddNote' onSubmit={e => this.handleSubmit(e)}>
         <h2>Create a Note</h2>
+        <NotefulForm onSubmit={this.handleSubmit}>
           <div className='form'>
             <label htmlFor='note-name-input'>
               Enter a note name: 
@@ -27,13 +60,21 @@ export default class AddFolder extends Component {
             <label htmlFor='folder-choice'>
                 Enter folder:
             </label>
-            <input type= 'text' id='folder-choice' name='folder-choice' />
+            <select id ='folder-choice' name='folder-choice'>
+              <option value ={null}>...</option>
+              {folders.map(folder =>
+                <option key={folder.id} value={folder.id}>
+                  {folder.name}
+                </option>
+              )}
+            </select>
           </div>
           <div className='button'>
             <button type='submit'>
 							Submit
             </button>
           </div>
+        </NotefulForm>
       </section>
     )
   }
